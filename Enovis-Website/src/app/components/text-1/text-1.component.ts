ffa9359dc6174b1f78119d1d668dcc70cc79193e
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-08-12 00:01:10 +0300

    added text-1 component
    added a text component containing 3 types of styling

---
+++ Enovis-Website/src/app/components/text-1/text-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-1.component.html',
+  styleUrl: './text-1.component.css'
+})
+export class Text1Component {
+
+}
