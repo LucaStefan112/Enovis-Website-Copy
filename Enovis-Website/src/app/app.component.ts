cf61e8b8c93c4fea2e404d52f001aab937f0c915
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-08-14 00:28:38 +0300

    added review-container component

--- Enovis-Website/src/app/app.component.ts
+++ Enovis-Website/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ImageTextBoxComponent } from './components/image-text-box/image-text-box.component';
 import { TextBoxComponent } from './components/text-box/text-box.component';
-import { ImageTextViewComponent } from './components/image-text-view/image-text-view.component';  // Added from Stashed changes
+import { ImageTextViewComponent } from './components/image-text-view/image-text-view.component';  
+import { ReviewContainerComponent } from './components/review-container/review-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, ImageTextBoxComponent, TextBoxComponent, ImageTextViewComponent ],  // Added ImageTextViewComponent to imports
+  imports: [ RouterOutlet, ImageTextBoxComponent, TextBoxComponent, ImageTextViewComponent, ReviewContainerComponent ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Fixed typo: styleUrl to styleUrls
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'Enovis-Website';
--- Enovis-Website/src/app/components/review-container/review-container.component.ts
+++ Enovis-Website/src/app/components/review-container/review-container.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-review-container',
   standalone: true,
   imports: [],
   templateUrl: './review-container.component.html',
-  styleUrl: './review-container.component.css'
+  styleUrl: './review-container.component.css',
 })
 export class ReviewContainerComponent {
-
+  @Input() text: string = '';
+  @Input() location: string = '';
+  @Input() name: string = '';
+  @Input() src: string = '';
 }
