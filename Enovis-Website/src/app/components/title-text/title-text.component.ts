9fd4a483501290095dbde7797545c0fbc52e56bd
Limit1500 <146538463+Limit1500@users.noreply.github.com>
2024-08-16 10:25:41 +0300

    added home page

--- Enovis-Website/src/app/components/title-text/title-text.component.ts
+++ Enovis-Website/src/app/components/title-text/title-text.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title-text',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './title-text.component.html',
   styleUrl: './title-text.component.css'
 })
 export class TitleTextComponent {
   @Input() title: string = '';
   @Input() text: string = '';
+  @Input() color: string = '';
 }
